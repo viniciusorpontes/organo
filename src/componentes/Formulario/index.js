28c77feaf0faf834087117192e234294e7a7d488
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-04-25 23:45:58 -0300

    Adicionando campo de lista suspensa, ajustando campos obrigatórios e adicionando botão para submissão do formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,36 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const salvar = (evento) => {
+        evento.preventDefault()
+        console.log("Form foi submetido")
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={salvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Nome" placeholder="Digite seu nome" obrigatorio={true} />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" obrigatorio={true} />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} obrigatorio={true} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
