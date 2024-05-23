30506f3ba020741b1351eb92f6b5b5777fd176ed
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-05-23 08:42:52 -0300

    Criando e passando função de salvar para o componente formulário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores , setColaboradores] = useState([])
+
+  const salvarColaborador = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+    console.log(colaboradores)
+  }
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario salvar={colaborador => salvarColaborador(colaborador)}/>
     </div>
   );
 }
