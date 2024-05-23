7ac0bbd4b9fe4fe59adb9c37f5e78d2496f18218
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-05-23 08:40:03 -0300

    Passando novas props value e função de alteração de campo para os inputs

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,7 +4,7 @@ const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} required={props.obrigatorio}/>
+            <input value={props.valor} onChange={evento => props.alterarCampo(evento.target.value)} placeholder={props.placeholder} required={props.obrigatorio}/>
         </div>
     )
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,7 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.required}>
+            <select value={props.valor} onChange={evento => props.alterarCampo(evento.target.value)} required={props.required}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
