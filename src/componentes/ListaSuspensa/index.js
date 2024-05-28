01d638296c6d0b8a03ae1c21344295a70b43174c
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2024-05-28 19:01:40 -0300

    Adicionando <option> vazio para ser possível limpar o formulário após o submit

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,7 +5,12 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select value={props.valor} onChange={evento => props.alterarCampo(evento.target.value)} required={props.required}>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
+                <option value=""></option>
+                {
+                    props.itens.map(item =>
+                        <option key={item}>{item}</option>
+                    )
+                }
             </select>
         </div>
     )
